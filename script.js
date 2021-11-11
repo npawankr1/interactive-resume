87201039612698c9ca327a7a43d2685f0291fc0c
npawankr1 <58778029+npawankr1@users.noreply.github.com>
2021-11-11 10:22:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let m = document.getElementById("mountain");
 let m0 = document.getElementById("mountain0");
 let m1 = document.getElementById("mountain1");
 let birdX = 50;
-let birdY = 0;
+let birdY = -10;
 let birdFrontBack = 10;
 let birdsSpeed = 2;
 let birds = document.getElementById("birds");
@@ -57,8 +57,20 @@ let hut1 = document.getElementById("hut1")
 let hut2 = document.getElementById("hut2")
 let hut3 = document.getElementById("hut3")
 let clgNew = document.getElementById("clgNew");
+let ufo = document.getElementById("ufo");
+let mark = document.getElementById("mark");
+
+let inst1 = document.getElementById("inst1");
+let inst2 = document.getElementById("inst2");
+inst1.innerHTML = "USE ARROW KEYS TO MOVE";
+
+let marksheet = document.getElementById("marksheet");
+let seacreatures = document.getElementById("seacreatures")
+let seacreaturesRev = document.getElementById("seacreaturesRev")
+let bls = document.getElementById("bls");
+
 let curHero = hero1;
-let heroX = 100;
+let heroX = 250;
 let heroY = 320;
 let move = 0;
 let plv = 40;
@@ -146,14 +158,14 @@ function flippingCoins(){
 }
 
 let blastCoordinates = [[1110,22],[910,22],[708,22]]
-let exploar = [[0,0,100,100,7995,200,100,100],[100,0,150,109,7970,205,150,100],[230,0,170,120,7980,210,150,100],[370,0,150,120,7990,210,130,100],[520,0,140,120,8005,210,130,100],[630,0,140,120,7990,210,130,100],[770,0,140,120,8000,210,130,100],[890,0,150,120,7990,210,140,100],[1040,0,120,120,8012,210,110,100],[1140,0,120,120,7985,210,110,100],[1265,0,150,120,7988,210,130,100],[1390,0,160,120,7988,212,140,100],[1530,0,150,120,7995,210,130,100],[1650,0,150,120,7989,210,130,100],[1790,0,110,120,7983,210,130,100],[1899,0,150,120,7983,210,130,100]];
+let exploar = [[0,0,100,100,8020,220,100,100],[100,0,150,109,8040,224,150,100],[155,0,160,120,8018,223,150,110],[228,0,130,125,8009,222,130-5,110],[320,0,150,130,8020,220,150,120],[400,0,130,120,8018,220,130,110],[570,0,130-5,120,8030,220,125,105],[650,0,130-5,120,8030,220,125,105],[720,0,130-5,120,8020,220,125,105],[820-10,0,130-5,120,8020+10,220,125,105],[920-30,0,130-5,120,8020+13,220,125,105],[1020-45,0,130-5,120,8020+15,220,125,105],[1120-60,0,130-5,120,8020+20,220,125,105],[1220-80,0,130-5,120,8020+20,220,125,105]];
 let cntrx = 0;
 let cntr = 0;
 let expoX;
 let expoY;
 function explode(expoX,expoY){
-	if (cntrx == 16) {
-		cntrx = 16
+	if (cntrx == 14) {
+		cntrx = 14
 	}else{
 		context.drawImage(explosion,exploar[cntrx][0],exploar[cntrx][1],exploar[cntrx][2],exploar[cntrx][3],exploar[cntrx][4]+expoX,exploar[cntrx][5]+expoY,exploar[cntrx][6],exploar[cntrx][7])
 		if (cntr%3 == 2) {
@@ -170,8 +182,8 @@ let expoB;
 let cntrA = 0;
 let ctrA = 0;
 function explodeA(expoA,expoB){
-	if (cntrA == 16) {
-		cntrx = 16
+	if (cntrA == 14) {
+		cntrx = 14
 	}else{
 		context.drawImage(explosion,exploar[cntrA][0],exploar[cntrA][1],exploar[cntrA][2],exploar[cntrA][3],exploar[cntrA][4]+expoA,exploar[cntrA][5]+expoB,exploar[cntrA][6],exploar[cntrA][7])
 		if (ctrA%3 == 2) {
@@ -188,8 +200,8 @@ let expoQ;
 let cntrP = 0;
 let ctrP = 0;
 function explodeP(expoP,expoQ){
-	if (cntrP == 16) {
-		cntrP = 16
+	if (cntrP == 14) {
+		cntrP = 14
 	}else{
 		context.drawImage(explosion,exploar[cntrP][0],exploar[cntrP][1],exploar[cntrP][2],exploar[cntrP][3],exploar[cntrP][4]+expoP,exploar[cntrP][5]+expoQ,exploar[cntrP][6],exploar[cntrP][7])
 		if (ctrP%3 == 2) {
@@ -259,6 +271,9 @@ function waterMovement(){
 		context.drawImage(blueWater,0,0,100,100,7600,580,300,100)
 		context.drawImage(blueWater,0,0,100,100,7900-1,580,300,100)
 		context.drawImage(blueWater,0,0,100,100,8200-2,580,300,100)
+		context.drawImage(blueWater,0,0,100,100,7600,660,300,100)
+		context.drawImage(blueWater,0,0,100,100,7900-1,660,300,100)
+		context.drawImage(blueWater,0,0,100,100,8200-2,660,300,100)
 	}else{
 		context.drawImage(blueWaterRev,0,0,100,100,7600,500,300,100)
 		context.drawImage(blueWaterRev,0,0,100,100,7900-1,500,300,100)
@@ -266,6 +281,10 @@ function waterMovement(){
 		context.drawImage(blueWaterRev,0,0,100,100,7600,580,300,100)
 		context.drawImage(blueWaterRev,0,0,100,100,7900-1,580,300,100)
 		context.drawImage(blueWaterRev,0,0,100,100,8200-2,580,300,100)
+		context.drawImage(blueWaterRev,0,0,100,100,7600,660,300,100)
+		context.drawImage(blueWaterRev,0,0,100,100,7900-1,660,300,100)
+		context.drawImage(blueWaterRev,0,0,100,100,8200-2,660,300,100)
+
 	}
 	waterCnt +=1
 }
@@ -303,25 +322,172 @@ function boyDown(){
 }
 let liveFlag = 0;
 
-function disableScroll() {
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-  
-        window.onscroll = function() {
-            window.scrollTo(scrollLeft, scrollTop);
-        };
-}
-  
-function enableScroll() {
-    window.onscroll = function() {};
+
+resX = 500;
+function showResult(){
+	if (resX!=200) {
+		resX -=10
+	}
+	context.drawImage(marksheet,4650,resX,150,200)
 }
 
 let variable = 8600;
 var inc = 50;
+
+
+var octoArr = [[345,215,50,80,8300,0,58,84],[230,115,50,60,8300-2,0.5,57+1,63],[573,730,80,73,8300-5,3,89+2,77-1],[344,915,54,60,8300-7,2,60+2,65]]
+let octCnt = 0;
+let dumOct = 0;
+let octDirec = 1;
+let octY = 650;
+let octYDirec = -1;
+function octopusMovement(){
+	if (dumOct%7 == 6) {
+		octCnt +=octDirec;
+		if (octCnt == 4) {
+			octCnt = 2
+			if (octDirec > 0) {
+				octDirec *=(-1)
+			}
+		}
+		if (octCnt == -1) {
+			octCnt = 1
+			if (octDirec < 0) {
+				octDirec *=(-1)
+			}
+		}
+	}
+	if (octY > 650) {
+		if (octYDirec > 0) {
+			octYDirec *=(-1)
+		}
+	}
+	if (octY < 550) {
+		if (octYDirec < 0) {
+			octYDirec *=(-1)
+		}
+	}
+	octY += octYDirec;
+	context.drawImage(seacreatures,octoArr[octCnt][0],octoArr[octCnt][1],octoArr[octCnt][2],octoArr[octCnt][3],octoArr[octCnt][4],octY+octoArr[octCnt][5],octoArr[octCnt][6],octoArr[octCnt][7]);
+	dumOct ++;
+	dumOct %=(7);
+}
+
+var jellyArr = [[0,484,58,115,7650,600,60,100],[60,484,58,115,7650+3-0.5,600,60,100],[120-5,484,58,115+2,7650,600-3,60,100+1.5]]
+let jellyCnt = 0;
+let jellyGap = 0;
+let jellyDirec = 1;
+function jellyMovement(){
+	if (jellyGap%5 == 4) {
+		jellyCnt +=jellyDirec;
+	}
+	if (jellyCnt == 3) {
+		jellyCnt = 1;
+		if (jellyDirec > 0) {
+			jellyDirec *=(-1)
+		}
+	}
+	if (jellyCnt == -1) {
+		jellyCnt = 1;
+		if (jellyDirec < 0) {
+			jellyDirec *=(-1)
+		}
+	}
+	context.drawImage(seacreatures,jellyArr[jellyCnt][0],jellyArr[jellyCnt][1],jellyArr[jellyCnt][2],jellyArr[jellyCnt][3],jellyArr[jellyCnt][4],jellyArr[jellyCnt][5],jellyArr[jellyCnt][6],jellyArr[jellyCnt][7]);
+	jellyGap ++;
+}
+
+let yellowFishX = 7990;
+let purpleFishX = 7690;
+let yellowFishDirec = 1;
+let purpleFishDirec = 2;
+function fishMovement(){
+	if (yellowFishX <= 7620) {
+		if (yellowFishDirec < 0) {
+			yellowFishDirec *=(-1)
+		}
+	}
+	if (yellowFishX >= 8400) {
+		if (yellowFishDirec > 0) {
+			yellowFishDirec *=(-1)
+		}
+	}
+	if (purpleFishX <= 7690) {
+		if (purpleFishDirec < 0) {
+			purpleFishDirec *=(-1)
+		}
+	}
+	if (purpleFishX >= 8000) {
+		if (purpleFishDirec > 0) {
+			purpleFishDirec *=(-1)
+		}
+	}
+	yellowFishX +=yellowFishDirec;
+	purpleFishX +=purpleFishDirec;
+	if (yellowFishDirec < 0) {
+		context.drawImage(seacreatures,390,215,100,102,yellowFishX,600,50,50);
+	}else{
+		context.drawImage(seacreaturesRev,100+50,205+10,100,102,yellowFishX,600,50,50);
+	}
+	if (purpleFishDirec < 0) {
+		context.drawImage(seacreatures,245,487,100,60,purpleFishX,650,80,50);
+	}else{
+		context.drawImage(seacreaturesRev,295,487,100,60,purpleFishX,650,80,50);
+	}
+
+}
+
+let bblSize = 50;
+let bblX = 8000;
+let bblY = 700;
+let sby = 618;
+let sby0 = bblY - 50;
+let sby1 = bblY-150;
+function bubbleMovement(){
+	if (bblY <= 400) {
+		bblY = 700;
+	}
+	if (sby <= 550) {
+		sby = 618;
+	}
+	if (sby0 <= 400) {
+		sby0 = 700-50;
+	}
+	if (sby1 <= 400) {
+		sby1 = 700-150;
+	}
+	bblY -=1;
+	sby -=1;
+	sby0 -=2;
+	sby1 -=1.5
+	context.drawImage(bls,bblX,bblY,bblSize,bblSize);
+	context.drawImage(bls,bblX-50,sby0,bblSize,bblSize);
+	context.drawImage(bls,7630,sby,25,25);
+	context.drawImage(bls,bblX,sby1,bblSize-10,bblSize-10);
+
+	context.drawImage(bls,bblX-50+200,sby0,bblSize,bblSize);
+	context.drawImage(bls,bblX+200,sby1,bblSize-10,bblSize-10);
+	context.drawImage(bls,bblX-50+350,sby0-100,bblSize,bblSize);	
+}
+
+
+
+let modeFlag = 0;
 function main(){
 	context.clearRect(0,0,gamebox.width, gamebox.height);
+	gamebox.height = window.innerHeight;
+	gamebox.style.background = 'url(https://i.ibb.co/VjSG0Wy/night.jpg)';
+	//gamebox.style.backgroundcolor = '#66ccff';
+	if (heroX >= 4300) {
+		showResult()
+	}
 	context.drawImage(clgNew,4105,75,500,450)
+	context.drawImage(mark,1400,390,170,110)
+	context.drawImage(ufo,950,57,170,170)
 	context.drawImage(collg,500,0,500,450)
+	context.drawImage(about,variable,100,300,400)
+	context.drawImage(about,variable+2100,100,300,400)
+	context.drawImage(about,variable+5500,100,300,400)
 	context.drawImage(intres,350,187,870,320)
 	context.drawImage(coco,5200,300,200,300)
 	context.drawImage(coco,5400,240,200,300)
@@ -335,7 +501,9 @@ function main(){
 	context.drawImage(longTree,7300,325,100,200)
 	context.drawImage(cvd,variable+400,200,450,300)
 	context.drawImage(tex,variable+270,50,450,250)
-	context.drawImage(dwarfTree,variable+2120,250,150,300)
+	context.drawImage(dwarfTree,variable+2020-150,250,150,300)
+	context.drawImage(about,1250,100,300,400)
+	context.drawImage(about,4800,100,300,400)
 	
 	if (heroX >= (6900-1)) {
 		flq1 = 1;
@@ -352,6 +520,7 @@ function main(){
 		q1h = 0;
 	}
 	context.drawImage(quesbox,6900,200,100,q1h)
+
 	if (expofl1 == 1) {
 		explode(-blastCoordinates[0][0],-blastCoordinates[0][1])
 	}
@@ -383,7 +552,7 @@ function main(){
 			bugMovement()
 		}
 	}
-	if ((bugX - heroX)==5) {
+	if ((bugX <= heroX)) {
 		bugFlag = 1
 		bugHeight = 1;
 	}
@@ -396,13 +565,13 @@ function main(){
 	context.drawImage(m0,1500+1650,220,400,280)
 	context.drawImage(m1,1850+1650,120,500,380)
 	context.drawImage(hometown,1590,270,500,350)
-	context.drawImage(dwarfTree,variable+5600,250,170,250)
+	context.drawImage(dwarfTree,variable+5400,250,170,250)
 	lv1.innerHTML = "Level 1";
 	abt.innerHTML = "About>>"
 	let dum = 0;
 	let inf = 1;
 	for (let i = 0; i <inf; i++){
-		context.drawImage(ground, 0+dum, 520,100,70);
+		context.drawImage(ground, 0+dum, 520+7,100,70);
 		dum +=100;
 		if (dum>=gamebox.width) {
 			break;
@@ -414,7 +583,7 @@ function main(){
 	var d = 0;
 	var ct = 0;
 	for(let x = 0; x < 500; x++){
-		context.drawImage(ground, 0+d, 590,100,70);
+		context.drawImage(ground, 0+d, 590+7,100,70);
 		if (ct%2 == 0) {
 			context.drawImage(cloud0,cdis-500,-20,130,130);
 		}
@@ -424,7 +593,7 @@ function main(){
 	}
 	var d = 0;
 	for(let x = 0; x < 500; x++){
-		context.drawImage(ground, 0+d, 660,100,70);
+		context.drawImage(ground, 0+d, 660+7,100,70);
 		d +=100;
 	}
 	if (heroX <= 0) {
@@ -435,11 +604,13 @@ function main(){
 		context.drawImage(grass, 0+d, 500,100,70);
 		d +=100;
 	}
-	context.drawImage(about,variable,100,300,400)
+	context.drawImage(coco,5600,200,200,300)
+	//context.drawImage(about,variable,100,300,400)
 	context.drawImage(pro0,variable+1100,200,600,300)
+	context.drawImage(seacreatures,344,915,54,60,variable+820,150,60+2,65)
 	context.drawImage(pro1,variable+1580,400,200,100)
 	context.drawImage(longTree,variable+2000,200,150,300)
-	context.drawImage(about,variable+2100,100,300,400)
+	//context.drawImage(about,variable+2100,100,300,400)
 	context.drawImage(cup,variable+2500,150,250,350)
 	context.drawImage(cn,variable+2750,200,200,100)
 	context.drawImage(cup,variable+3100,150,250,350)
@@ -447,8 +618,8 @@ function main(){
 	context.drawImage(zeal,variable+4400,300,600,200)
 	context.drawImage(snackdown,variable+5200,400,300,100)
 	context.drawImage(banTree,variable+6000,15,400,500)
-	context.drawImage(longTree,variable+5500,200,150,300)
-	context.drawImage(about,variable+5500,100,300,400)
+	context.drawImage(longTree,variable+5900-100,100,200,400)
+	//context.drawImage(about,variable+5500,100,300,400)
 	context.drawImage(sun,variable+6400-250-20,-10+120+20,150,150)
 	context.drawImage(manLogo,variable+6423-250-20,7+120+20,100,100)
 	context.drawImage(hut1,variable+3350,155,250,350)
@@ -471,11 +642,11 @@ function main(){
 		live(200);
 	}
 	waterMovement();
+	fishMovement();
+	octopusMovement();
+	jellyMovement();
+	bubbleMovement();
 	context.drawImage(motorBoat,boatX,380,300,200)
-	context.drawImage(about,1250,100,300,400)
-	context.drawImage(about,4800,100,300,400)
-	
-	context.drawImage(coco,5600,200,200,300)
 	
 	if (heroX >= (variable + 6800)) {
 		heroX = variable + 6800;
